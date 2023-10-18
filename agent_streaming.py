99587c22354939eaba167cea48089bddb97ee66c
Madhao Wagh <madhaowagh@Mukunds-MacBook-Pro.local>
2023-10-17 20:28:45 -0700

    updating requirements

--- agent_streaming.py
+++ agent_streaming.py
@@ -13,7 +13,7 @@ class StreamHandler(BaseCallbackHandler):
     def __init__(self, container, initial_text=""):
         self.container = container
         self.text = initial_text
-
+        
     def on_llm_new_token(self, token: str, **kwargs) -> None:
         self.text += token
         self.container.markdown(self.text)
